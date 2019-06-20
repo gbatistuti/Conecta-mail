3f3e6b63f7d4078a61388b3cc67a482a52aec836
gbatistuti <gustavo.batistuti1@gmail.com>
2019-06-20 16:29:30 -0300

    Alteração do TimerTask pela notação do Spring @Scheduled

--- conecta-mail/src/main/java/br/com/projeto/conecta/mail/ConectaMailApplication.java
+++ conecta-mail/src/main/java/br/com/projeto/conecta/mail/ConectaMailApplication.java
@@ -1,96 +1,13 @@
 package br.com.projeto.conecta.mail;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.http.ResponseEntity;
-
-import br.com.projeto.conecta.mail.domain.Mensagem;
-import br.com.projeto.conecta.mail.service.MensageiroService;
-import br.com.projeto.conecta.mail.service.MensagemService;
 
 @SpringBootApplication
 public class ConectaMailApplication {
-	
-//	public static void main(String[] args) {
-//		SpringApplication.run(ConectaMailApplication.class, args);
-//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-//
-//				ConectaMailApplication.class.getPackage().getName());
-//
-//		MensageiroService mensageiro = applicationContext.getBean(MensageiroService.class);
-//
-//		MensagemService mensagemComponent = new MensagemService();
-//
-//		ResponseEntity<Mensagem> resposta = mensagemComponent.getRestTemplate().getForEntity(mensagemComponent.getUrl(),
-//				Mensagem.class);
-//
-//		if(resposta != null) {
-//		Mensagem msg = resposta.getBody();
-//		
-//		mensageiro.enviar(msg);
-//		
-//		System.out.println("Enviou :)");
-//		}
-//		applicationContext.close();
-//
-//		System.out.println("Fim!");
-//	}
-
-//	public static void main(String[] args) {
-//		SpringApplication.run(ConectaMailApplication.class, args);
-//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-//				ConectaMailApplication.class.getPackage().getName());
-//
-//		Timer timer = new Timer();
-//		ConectaMailApplication conectaMailApplication = new ConectaMailApplication();
-//		timer.schedule(conectaMailApplication, 0, 1000);
-//
-//	}
-//
-//	@Override
-//	public void run() {
-//		
-//		MensageiroService mensageiro = new MensageiroService();
-//		
-//		MensagemService mensagemComponent = new MensagemService();
-//		ResponseEntity<Mensagem> resposta = mensagemComponent.getRestTemplate().getForEntity(mensagemComponent.getUrl(),
-//				Mensagem.class);
-//		System.out.println("TA RODANDO");
-//		if (resposta.hasBody()) {
-//			System.out.println("ENTROU NO IF -------");
-//			Mensagem msg = resposta.getBody();
-//			
-//			mensageiro.enviar(msg);
-//			
-//			System.out.println("Enviou :)");
-//		}
-//		
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConectaMailApplication.class, args);
-//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
-//
-//				ConectaMailApplication.class.getPackage().getName());
-
-		//MensageiroService mensageiro = applicationContext.getBean(MensageiroService.class);
-
-//		MensagemService mensagemService = new MensagemService();
-//		
-//		mensagemService.enviarMensagemApontamento();
-		
-		Timer timer = new Timer();
-		TimerTask mensagemService = new MensagemService();
-		timer.schedule(mensagemService, 0, 10000);
-
-		
-		//applicationContext.close();
-
 		
 	}
-
 }
--- conecta-mail/src/main/java/br/com/projeto/conecta/mail/service/MensagemService.java
+++ conecta-mail/src/main/java/br/com/projeto/conecta/mail/service/MensagemService.java
@@ -1,47 +1,41 @@
 package br.com.projeto.conecta.mail.service;
 
-import java.util.TimerTask;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.projeto.conecta.mail.domain.Mensagem;
 
 @Component
-public class MensagemService extends TimerTask {
+@EnableScheduling
+public class MensagemService {
 
 	@Autowired
 	MensageiroService mensageiroService;
 
 	private final RestTemplate restTemplate;
 	private final String url;
-	private final ResponseEntity<Mensagem> resposta;
 
 	public MensagemService() {
 		this.restTemplate = new RestTemplate();
 		this.url = "http://localhost:8080/mensagem";
-		this.resposta = this.restTemplate.getForEntity(url, Mensagem.class);
 	}
 
-	@Autowired
-	@Override
-	public void run() {
+	@Scheduled(fixedDelay = 1000)
+	public void enviarMensagem() {
 		
+		ResponseEntity<Mensagem> resposta = this.restTemplate.getForEntity(url, Mensagem.class);
 		
 		System.out.println("TA RODANDO");
 		if (resposta.hasBody()) {
 			System.out.println("------TEM CORPO-------");
 			Mensagem msg = resposta.getBody();
-			Mensagem msg2 = new Mensagem("gustavo.batistuti1@gmail.com", "TESTE", "TESTE");
 			try {
 				System.out.println("------TRY-------");
-				System.out.println(msg.getDestinatarios());
-				System.out.println(msg.getAssunto());
-				System.out.println(msg.getCorpo());
-				System.out.println(msg.toString());
 				mensageiroService.enviar(msg);
 				System.out.println("---------------DEU BOM------------");
 				ResponseEntity.ok("Mensagem enviada com sucesso!");
