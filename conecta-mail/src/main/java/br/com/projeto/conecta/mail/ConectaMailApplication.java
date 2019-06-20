ca917d970b5749cc9be8f5226d43c8fe102fbcb5
gbatistuti <gustavo.batistuti1@gmail.com>
2019-06-19 22:13:53 -0300

    Comunicação com a aplicação principal
    Com utilização de TimerTask para solicitar todas as mensagens que devem
    ser enviadas

--- conecta-mail/src/main/java/br/com/projeto/conecta/mail/ConectaMailApplication.java
+++ conecta-mail/src/main/java/br/com/projeto/conecta/mail/ConectaMailApplication.java
@@ -1,5 +1,8 @@
 package br.com.projeto.conecta.mail;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,30 +14,83 @@ import br.com.projeto.conecta.mail.service.MensagemService;
 
 @SpringBootApplication
 public class ConectaMailApplication {
+	
+//	public static void main(String[] args) {
+//		SpringApplication.run(ConectaMailApplication.class, args);
+//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+//
+//				ConectaMailApplication.class.getPackage().getName());
+//
+//		MensageiroService mensageiro = applicationContext.getBean(MensageiroService.class);
+//
+//		MensagemService mensagemComponent = new MensagemService();
+//
+//		ResponseEntity<Mensagem> resposta = mensagemComponent.getRestTemplate().getForEntity(mensagemComponent.getUrl(),
+//				Mensagem.class);
+//
+//		if(resposta != null) {
+//		Mensagem msg = resposta.getBody();
+//		
+//		mensageiro.enviar(msg);
+//		
+//		System.out.println("Enviou :)");
+//		}
+//		applicationContext.close();
+//
+//		System.out.println("Fim!");
+//	}
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(ConectaMailApplication.class, args);
+//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+//				ConectaMailApplication.class.getPackage().getName());
+//
+//		Timer timer = new Timer();
+//		ConectaMailApplication conectaMailApplication = new ConectaMailApplication();
+//		timer.schedule(conectaMailApplication, 0, 1000);
+//
+//	}
+//
+//	@Override
+//	public void run() {
+//		
+//		MensageiroService mensageiro = new MensageiroService();
+//		
+//		MensagemService mensagemComponent = new MensagemService();
+//		ResponseEntity<Mensagem> resposta = mensagemComponent.getRestTemplate().getForEntity(mensagemComponent.getUrl(),
+//				Mensagem.class);
+//		System.out.println("TA RODANDO");
+//		if (resposta.hasBody()) {
+//			System.out.println("ENTROU NO IF -------");
+//			Mensagem msg = resposta.getBody();
+//			
+//			mensageiro.enviar(msg);
+//			
+//			System.out.println("Enviou :)");
+//		}
+//		
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConectaMailApplication.class, args);
-		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+//		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+//
+//				ConectaMailApplication.class.getPackage().getName());
 
-				ConectaMailApplication.class.getPackage().getName());
+		//MensageiroService mensageiro = applicationContext.getBean(MensageiroService.class);
 
-		MensageiroService mensageiro = applicationContext.getBean(MensageiroService.class);
-
-		MensagemService mensagemComponent = new MensagemService();
-
-		ResponseEntity<Mensagem> resposta = mensagemComponent.getRestTemplate().getForEntity(mensagemComponent.getUrl(),
-				Mensagem.class);
-
-		if(resposta != null) {
-		Mensagem msg = resposta.getBody();
+//		MensagemService mensagemService = new MensagemService();
+//		
+//		mensagemService.enviarMensagemApontamento();
 		
-		mensageiro.enviar(msg);
-		
-		System.out.println("Enviou :)");
-		}
-		applicationContext.close();
+		Timer timer = new Timer();
+		TimerTask mensagemService = new MensagemService();
+		timer.schedule(mensagemService, 0, 10000);
 
-		System.out.println("Fim!");
+		
+		//applicationContext.close();
+
+		
 	}
 
 }
--- conecta-mail/src/main/java/br/com/projeto/conecta/mail/service/MensagemService.java
+++ conecta-mail/src/main/java/br/com/projeto/conecta/mail/service/MensagemService.java
@@ -1,5 +1,7 @@
 package br.com.projeto.conecta.mail.service;
 
+import java.util.TimerTask;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,46 +11,52 @@ import org.springframework.web.client.RestTemplate;
 import br.com.projeto.conecta.mail.domain.Mensagem;
 
 @Component
-public class MensagemService {
-	
+public class MensagemService extends TimerTask {
+
 	@Autowired
 	MensageiroService mensageiroService;
-	
+
 	private final RestTemplate restTemplate;
 	private final String url;
-	
+	private final ResponseEntity<Mensagem> resposta;
+
 	public MensagemService() {
 		this.restTemplate = new RestTemplate();
 		this.url = "http://localhost:8080/mensagem";
+		this.resposta = this.restTemplate.getForEntity(url, Mensagem.class);
 	}
-	
-	
+
+	@Autowired
+	@Override
+	public void run() {
+		
+		
+		System.out.println("TA RODANDO");
+		if (resposta.hasBody()) {
+			System.out.println("------TEM CORPO-------");
+			Mensagem msg = resposta.getBody();
+			Mensagem msg2 = new Mensagem("gustavo.batistuti1@gmail.com", "TESTE", "TESTE");
+			try {
+				System.out.println("------TRY-------");
+				System.out.println(msg.getDestinatarios());
+				System.out.println(msg.getAssunto());
+				System.out.println(msg.getCorpo());
+				System.out.println(msg.toString());
+				mensageiroService.enviar(msg);
+				System.out.println("---------------DEU BOM------------");
+				ResponseEntity.ok("Mensagem enviada com sucesso!");
+			} catch (Exception e) {
+				System.out.println("------CATCH-------");
+				ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro interno no servidor");
+			}
+		}
+	}
+
 	public RestTemplate getRestTemplate() {
 		return restTemplate;
 	}
 
-
 	public String getUrl() {
 		return url;
 	}
-
-
-//	public ResponseEntity<String> enviarMensagemApontamento() {
-//		ResponseEntity<Mensagem> resposta = this.restTemplate.getForEntity(url, Mensagem.class);
-//		
-//		Mensagem msg = resposta.getBody();
-//		
-//		try {
-//			mensageiroService.enviar(msg);
-//			return ResponseEntity.ok("Mensagem enviada com sucesso!");
-//		} catch (Exception e) {
-//			return ResponseEntity
-//					.status(HttpStatus.INTERNAL_SERVER_ERROR)
-//					.body("erro interno no servidor");
-//		}	
-//		
-//	}
-	
-	
-
 }
